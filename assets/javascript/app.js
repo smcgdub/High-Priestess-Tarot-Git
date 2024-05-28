c7c89082f014688fb9f8caf75c054ff478691ad7
Stephen <73615623+smcgdub@users.noreply.github.com>
2024-05-28 11:02:11 +0100

    Created the reading card options on the contact us page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,19 @@
 // Animate on scroll JS
 AOS.init();
+
+// Scroll to top button
+function scrollFunction() {
+  var scrollToTopButton = document.getElementById("scrollToTopButton");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
