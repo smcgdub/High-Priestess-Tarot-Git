8a8dc7693337aa6705e6493245411d5b591f77ea
Stephen <73615623+smcgdub@users.noreply.github.com>
2024-06-13 15:58:58 +0100

    Added comments to JS code in app.js file.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,31 @@
-// Scroll to top button JS code
+// Attach an event handler to the window's scroll event
 window.onscroll = function () {
+  // Call the scrollFunction whenever a scroll event occurs
   scrollFunction()
 };
 
-// Scroll to top button
+// Define the scrollFunction
 function scrollFunction() {
+  // Get the scrollToTopButton element from the DOM
   var scrollToTopButton = document.getElementById("scrollToTopButton");
+  // Check if the scroll position of the body or the documentElement (html) is more than 20
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    // If it is, display the scrollToTopButton
     scrollToTopButton.style.display = "block";
   } else {
+    // If it's not, hide the scrollToTopButton
     scrollToTopButton.style.display = "none";
   }
 }
 
+// Define the scrollToTop function
 function scrollToTop() {
+  // Scroll to the top of the window smoothly
   window.scrollTo({
     top: 0,
     behavior: 'smooth'
   });
 }
 
-// Animate on scroll JS
+// Initialize the Animate On Scroll library
 AOS.init();
